72cc563cb517779d68643bbad1f973f311e587b5
swimmingRiver <swimmingr@gmail.com>
2024-08-05 16:11:27 +0900

    영화 상세 정보 페이지 추가&& 링크 설정

--- client/src/app/_components/ranking/rankingList.tsx
+++ client/src/app/_components/ranking/rankingList.tsx
@@ -5,7 +5,7 @@ import RankingItem from "./rankingItem";
 import { getMovieRanking } from "@/app/api/getMovieRanking";
 import { useQuery } from "@tanstack/react-query";
 import { IRankingItem } from "@/app/types/movieRank";
-
+import Link from "next/link";
 const RankingList = () => {
   const { data: movieRankList } = useQuery({
     queryKey: ["movieRank"],
@@ -16,14 +16,17 @@ const RankingList = () => {
   return (
     <div className={style.ranking_wrapper}>
       <div>Ranking</div>
+
       <div className={style.ranking_list}>
         {movieRankList?.map((i: IRankingItem) => (
-          <RankingItem
-            key={i.movieCd}
-            rank={i.rank}
-            rankInten={i.rankInten}
-            movieNm={i.movieNm}
-          />
+          <Link href={`movie_info/${i.movieCd}`}>
+            <RankingItem
+              key={i.movieCd}
+              rank={i.rank}
+              rankInten={i.rankInten}
+              movieNm={i.movieNm}
+            />
+          </Link>
         ))}
       </div>
     </div>
