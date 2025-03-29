c6a8f74b65669d2c81007e8c65675c760826de6e
swimmingRiver <swimmingr@gmail.com>
2025-03-29 14:47:13 +0900

    style: 영화 상세 정보 간격 조정 및 배우 더보기 추가

--- client/src/app/movie_info/[movieId]/page.tsx
+++ client/src/app/movie_info/[movieId]/page.tsx
@@ -5,42 +5,57 @@ import Chart from "@/app/_components/chart";
 import { useParams } from "next/navigation";
 import { useQuery } from "@tanstack/react-query";
 import getMovieInfo from "@/app/api/getMovieInfo";
+
 const Page = () => {
   const { movieId } = useParams();
-
+  const [showAllActors, setShowAllActors] = useState(false);
   const [tab, setTab] = useState("줄거리");
+
   const { data } = useQuery({
     queryKey: ["movieInfo", movieId],
     queryFn: () => getMovieInfo(movieId as string),
   });
+
   interface PeopleInfo {
     peopleNm: string;
     peopleNmEn: string;
   }
 
+  const displayedActors = showAllActors 
+    ? data?.movieInfoResult.movieInfo.actors 
+    : data?.movieInfoResult.movieInfo.actors.slice(0, 2);
+
   return (
     <div className={style.movie_info_wrapper}>
       <div className={style.movie_info_content}>
         <div className={style.movie_info_title}>
-          <div>
+          <div style={{display:"flex",  gap:"4px"}}>
             {data?.movieInfoResult.movieInfo.movieNm}
             ({data?.movieInfoResult.movieInfo.prdtYear})
           </div>
-          <div>
+          <div style={{display:"flex",  gap:"4px"}}>
             <span>개봉일</span>
             {data?.movieInfoResult.movieInfo.openDt}
           </div>
-          <div>
+          <div style={{display:"flex",  gap:"4px"}}>
             <span>연출</span>
             {data?.movieInfoResult.movieInfo.directors.map((director:PeopleInfo) => (
               <div key={director.peopleNm}>{director.peopleNm}</div>
             ))}
           </div>
-          <div>
+          <div style={{display:"flex",  gap:"4px"}}>
             <span>출연</span>
-            {data?.movieInfoResult.movieInfo.actors.map((actor:PeopleInfo) => (
-              <div key={actor.peopleNm}>{actor.peopleNm}</div>
+            {displayedActors?.map((actor:PeopleInfo) => (
+              <div key={actor.peopleNm} style={{display:"inline-block"}}>{actor.peopleNm}</div>
             ))}
+            {data?.movieInfoResult.movieInfo.actors.length > 2 && (
+              <button 
+                onClick={() => setShowAllActors(!showAllActors)}
+                className={style.more_button}
+              >
+                {showAllActors ? '접기' : '더보기'}
+              </button>
+            )}
           </div>
         </div>
         <img
