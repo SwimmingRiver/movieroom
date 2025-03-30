39318278917083403613ba988968c13861311a81
swimmingRiver <swimmingr@gmail.com>
2025-03-30 12:54:38 +0900

    fix: 불필요한 코드 제거

--- client/src/app/_components/chart/Chart.tsx
+++ client/src/app/_components/chart/Chart.tsx
@@ -1,4 +1,4 @@
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const Chart = () => {
   const data = [
